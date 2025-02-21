a14ccc89285a5e9ce0b81f7e890e276f1faba095
Goodluck117 <patrickanthony028@gmail.com>
2025-02-21 00:35:34 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `[OWNER NAME]`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `[OpuhConnect]`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `[Be professional, supportive, empathy, detail-oriented, direct but friendly]`;
 export const AI_ROLE: string = `[AI ROLE]`;
