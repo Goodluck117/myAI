4c2146146b0228bb1a7b564ce1893cafb5128f07
Goodluck117 <patrickanthony028@gmail.com>
2025-02-27 23:25:51 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[PatoAI]`;
+export const OWNER_NAME: string = `Patrick Anthony`;
 export const OWNER_DESCRIPTION: string = `[Healthcare professional who is passionate about improving patients care]`;
 
-export const AI_NAME: string = `[OpuhConnect]`;
-export const AI_TONE: string = `[Be professional, supportive, empathy, detail-oriented, direct but friendly]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `PatoAI`;
+export const AI_TONE: string = `PatoAI`;
+export const AI_ROLE: string = `PatoAI`;
