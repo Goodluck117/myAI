cad0d8ecf9e9246001ed7148b8c134588f4df86f
Patrick A <patrickanthony028@gmail.com>
2025-03-06 16:31:55 -0500

    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -47,7 +47,7 @@ Use the following excerpts from ${OWNER_NAME} to answer the user's question. If 
 Excerpts from ${OWNER_NAME}:
 ${context}
 
-If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "I'm excited you asked about this! Here's what I can tell you..." then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
 
 Now respond to the user's message:
 `;
@@ -57,7 +57,7 @@ export function RESPOND_TO_QUESTION_BACKUP_SYSTEM_PROMPT() {
   return `
 ${IDENTITY_STATEMENT} ${OWNER_STATEMENT} ${OWNER_DESCRIPTION} ${AI_ROLE}
 
-You couldn't perform a proper search for the user's question, but still answer the question starting with "While I couldn't perform a search due to an error, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+You couldn't perform a proper search for the user's question, but still answer the question starting with "I'm excited you asked about this! Here's what I can tell you..." then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
 
 Now respond to the user's message:
 `;
