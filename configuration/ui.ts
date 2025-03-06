8c88cf65b7183b0391ab2bf8fd25a51736ca6d7f
Patrick A <patrickanthony028@gmail.com>
2025-03-06 17:56:42 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `(Disclaimer): Please verify any information you are getting on this site with your healthcare provider`;
 export const CLEAR_BUTTON_TEXT: string = `CLEAR BUTTON`;
 export const PAGE_TITLE: string = `Patrick's Diabetes and Obesity AI Assistant`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.  I specialize in diabetes and obesity—whether you need insights, guidance, or support. But don't hesitate to ask me about anything else—I'm here to help!`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
